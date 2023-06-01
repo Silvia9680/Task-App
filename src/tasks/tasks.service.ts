f929e43823b158632ca19cd4af88437d5a197e3e
Silvia9680~ <silviacarolina9680@gmail.com>
2023-06-01 10:16:58 -0300

    cambios por la DB

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,4 +1,30 @@
 import { Injectable } from '@nestjs/common';
+import {InjectModel} from '@nestjs/mongoose'
+import {Task} from '../schemas/task.schemas'
+import { Model} from 'mongoose'
 
 @Injectable()
-export class TasksService {}
+export class TasksService {
+    constructor(@InjectModel(Task.name) private taskModel:Model<Task> ){}
+
+    finAll() {
+        this.taskModel.find();
+    }
+
+    async create(createTask: any){
+        const newTask = new this.taskModel(createTask);
+        return newTask.save();
+    }
+
+    async findOne(id: string) {
+        return this.taskModel.findById(id);
+    }
+
+    async delete (id: string) {
+        return this.taskModel.findByIdAndDelete(id);
+    }
+
+    async update(id:string, task: any){
+        return this.taskModel.findByIdAndUpdate(id, task)
+    }
+}
